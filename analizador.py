c88cb30893ea1df7f95d572df31b54650f4f7064
Danny Berroa <tucorreo@example.com>
2025-08-11 12:35:33 -0400

    Agregado encabezado y librerías

---
+++ analizador.py
@@ -0,0 +1,10 @@
+# Autor: Danny Berroa Mercedes
+# Matrícula: 23-EISN-2-022
+# Descripción: Analizador de letras de canciones rápido con análisis de palabras usando Faster Whisper y Gradio
+
+from faster_whisper import WhisperModel
+import gradio as gr
+import time
+import nltk
+from collections import Counter
+
